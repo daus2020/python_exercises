aeb3ce842e3075caad120b4bcab7878257e9e258
daniel <danielandresus@gmail.com>
2023-05-03 20:41:37 -0400

    ex_08 added to menu

--- ex_08_v02.py
+++ ex_08_v02.py
@@ -1,6 +1,4 @@
-txt = "Hola cómo estás."
-# txt = "Hola cómo estás."
-# txt = "Hola cómo estás?"
+txt = "Hola cómo estás?"
 # txt = input("Ingrese Texto: ")
 letters = input("Ingrese palabra o cadena a buscar: ")
 
@@ -11,7 +9,9 @@ print(
 print(
     "2 > Palabra/cadena exacta (case sensitive), puede estar seguida sólo de espacio(s) o nada"
 )
-print("3 > Palabra/cadena con o sin acento  (case sensitive)")
+print(
+    "3 > Palabra/cadena con o sin acento  (case sensitive), puede estar seguida de cualquier caracter"
+)
 print(
     "4 > Palabra/cadena indistintamente mayúscula/minúscula (case insensitive), respetando acentos"
 )
@@ -21,7 +21,6 @@ print(
 search_option = input("Seleccione su opción de búsqueda: ")
 
 my_split = txt.split()
-print(f"my_split 27 -> {my_split}")
 
 first_string = "áéíóú"
 second_string = "aeiou"
@@ -33,21 +32,19 @@ letters_trans = letters.maketrans(first_string, second_string)
 letters_no_accents = letters.translate(letters_trans)
 letters_lower_no_accents = letters_no_accents.lower()
 
-
 txt_lower = txt.lower()
 letters_lower = letters.lower()
 
-
 if (letters in txt) and (search_option == "1"):
-    print(f"La cadena/palabra '{letters}' se encuentra en el texto 51")
+    print(f"La cadena/palabra '{letters}' se encuentra en el texto")
 elif (letters in txt) and (letters in my_split) and search_option == "2":
     print(f"La cadena '{letters}' se encuentra en el texto 53")
 elif (letters_no_accents in txt_no_accents) and (search_option == "3"):
-    print(f"La cadena/palabra '{letters}' se encuentra en el texto 55")
+    print(f"La cadena/palabra '{letters}' se encuentra en el texto")
 elif (letters_lower in txt_lower) and (search_option == "4"):
-    print(f"La cadena/palabra '{letters}' se encuentra en el texto 57")
+    print(f"La cadena/palabra '{letters}' se encuentra en el texto")
 # elif (letters in txt_lower_no_accents) and (search_option == "5"):
 elif (letters_lower_no_accents in txt_lower_no_accents) and (search_option == "5"):
-    print(f"La cadena/palabra '{letters}' se encuentra en el texto 60")
+    print(f"La cadena/palabra '{letters}' se encuentra en el texto")
 else:
     print(f"La palabra '{letters}' no se encuentra en el texto")
--- in_range.py
+++ in_range.py
@@ -1,9 +1,12 @@
-import re
+import re  # for validate run input (exercise 10)
+from rich.console import Console
+
+console = Console()
 
 
 # option 1:
 def average_mark():
-    print("Average Mark")
+    console.print("Average Mark", style="blue")
     marks = []
     while True:
         # my_mark = input("Ingrese nota (1.0 a 7.0): ")
@@ -112,7 +115,56 @@ def skip_number():
 
 # option 8:
 def is_word_in_txt():
-    print("pend")
+    txt = "Hola cómo estás?"
+    # txt = input("Ingrese Texto: ")
+    letters = input("Ingrese palabra o cadena a buscar: ")
+
+    print("Opciones de búsqueda")
+    print(
+        "1 > Palabra/cadena exacta (case sensitive), puede estar seguida de cualquier caracter ('',.!?etc.)"
+    )
+    print(
+        "2 > Palabra/cadena exacta (case sensitive), puede estar seguida sólo de espacio(s) o nada"
+    )
+    print(
+        "3 > Palabra/cadena con o sin acento  (case sensitive), puede estar seguida de cualquier caracter"
+    )
+    print(
+        "4 > Palabra/cadena indistintamente mayúscula/minúscula (case insensitive), respetando acentos"
+    )
+    print(
+        "5 > Palabra/cadena indistintamente mayúscula/minúscula (case insensitive),  con o sin acento"
+    )
+    search_option = input("Seleccione su opción de búsqueda: ")
+
+    my_split = txt.split()
+
+    first_string = "áéíóú"
+    second_string = "aeiou"
+
+    txt_trans = txt.maketrans(first_string, second_string)
+    txt_no_accents = txt.translate(txt_trans)
+    txt_lower_no_accents = txt_no_accents.lower()
+    letters_trans = letters.maketrans(first_string, second_string)
+    letters_no_accents = letters.translate(letters_trans)
+    letters_lower_no_accents = letters_no_accents.lower()
+
+    txt_lower = txt.lower()
+    letters_lower = letters.lower()
+
+    if (letters in txt) and (search_option == "1"):
+        print(f"La cadena/palabra '{letters}' se encuentra en el texto")
+    elif (letters in txt) and (letters in my_split) and search_option == "2":
+        print(f"La cadena '{letters}' se encuentra en el texto 53")
+    elif (letters_no_accents in txt_no_accents) and (search_option == "3"):
+        print(f"La cadena/palabra '{letters}' se encuentra en el texto")
+    elif (letters_lower in txt_lower) and (search_option == "4"):
+        print(f"La cadena/palabra '{letters}' se encuentra en el texto")
+    # elif (letters in txt_lower_no_accents) and (search_option == "5"):
+    elif (letters_lower_no_accents in txt_lower_no_accents) and (search_option == "5"):
+        print(f"La cadena/palabra '{letters}' se encuentra en el texto")
+    else:
+        print(f"La palabra '{letters}' no se encuentra en el texto")
     print("\n")
 
 
